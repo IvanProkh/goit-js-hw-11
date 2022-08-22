efb1ba4d70f02219097a55d309303c2f898780ae
IvanProkhorets <prohorets.ivan@gmail.com>
2022-08-22 23:57:44 +0300

    update

--- src/index.js
+++ src/index.js
@@ -54,27 +54,23 @@ async function fetchImages(nameSearch) {
   console.log('totalHits', totalHits);
   console.log('currentPage', currentPage);
 
-  if (totalHits > 0 && currentPage === 1) {
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    refs.loadMoreButton.classList.toggle('is-hidden');
-  }
-
   if (totalHits === 0) {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+  } else if (totalHits > 0 && currentPage === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    refs.loadMoreButton.classList.toggle('is-hidden');
+  } else if (totalHits === numOfElements && totalHits > 0) {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.loadMoreButton.classList.toggle('is-hidden');
   }
 
   if (totalHits !== numOfElements) {
     currentPage += 1;
     return response;
-  }
-
-  if (totalHits === numOfElements && totalHits > 0) {
-    Notiflix.Notify.failure(
-      "We're sorry, but you've reached the end of search results."
-    );
-    refs.loadMoreButton.classList.toggle('is-hidden');
   }
 }
 
